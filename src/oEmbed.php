a93bde2d3e9f8585caf94549d32b0ce9de23774b
Akash Bose <boseakash7@gmail.com>
2016-01-15 23:02:14 +0530

    licence header added.

--- src/oEmbed.php
+++ src/oEmbed.php
@@ -1,4 +1,28 @@
 <?php
+
+/* 
+ * Copyright (C) 2016 Ullashnetwork
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Main class of PHPOEmbed.
+ * 
+ * @author Akash Bose <bose@ullashkolkata.com>
+ * @since Jan 15, 2016 [10:55:12 PM]
+ */
 class PHPOEmbed{
   
   private $url;  
@@ -493,7 +517,12 @@ class PHPOEmbed{
   
 }
 
-
+/**
+ * Provider class of PHPOEmbed.
+ * 
+ * @author Akash Bose <bose@ullashkolkata.com>
+ * @since Jan 15, 2016 [10:56:13 PM]
+ */
 class PHPOEmbedProvider{
 
   private $api;
